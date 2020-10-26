1eaab8bcc058cc7456099c70d61e3f225264b44c
straiffix <straiffix@gmail.com>
2020-10-26 18:38:53 +0300

    Validate forms, adding new select input

--- static/main.js
+++ static/main.js
@@ -1,65 +1,38 @@
-/*function checkValide(
-        element;
-        alert_element;
-        valideFunction;){
-    if valideFunction(element)    
+const everythingAlright = new Array(6).fill(false);
+const regexLetters = /^[a-zA-Z]+$/;
+const submit = document.getElementById("submit");
+var originValue = null;
 
-}*/
-
-
-function attach_events(){
-
-    const everythingAlright = new Array(6).fill(false);
-    const submit = document.getElementById("submit");
-    const regexLetters = /^[a-zA-Z]+$/;
-    everythingAlright[3] = true;
-
-    const alert_name = document.getElementById("alert_name");
-    
-    const firstname = document.getElementById("firstname");
-    firstname.addEventListener("change", function(ev){
-        if(!regexLetters.test(firstname.value)){
-        firstname.className = "error";
-        alert_name.style.display = "block";
-        alert_name.innerHTML = "Wrong name. Should contain only letters";
-        everythingAlright[0] = false;
+function checkValide(
+        element,
+        count,
+        alert_element,
+        message,
+        valideFunction){
+    if (!valideFunction(element.value)){
+        
+        element.className = "error";
+        alert_element.style.display = "block";
+        alert_element.innerText = message;
+        everythingAlright[count] = false;
         submit.setAttribute("disabled", "");
+    }
+    else{
+        element.className = "alright";
+        alert_element.style.display = "none";
+        everythingAlright[count] = true;
+        if(everythingAlright.every((v) => v === true)){
+            submit.removeAttribute("disabled");
         }
-        else{
-            firstname.className = "alright";
-            alert_name.style.display = "none";
-            everythingAlright[0] = true;
-            if(everythingAlright.every((v) => v === true)){
-                submit.removeAttribute("disabled");
-            }
-        }
-    });
-    
-    const alert_lastname = document.getElementById("alert_lastname");
-    const lastname = document.getElementById("lastname");
-    lastname.addEventListener("change", function(ev){
-        if(!regexLetters.test(lastname.value)){
-        lastname.className = "error";
-        alert_lastname.style.display = "block";
-        alert_lastname.innerHTML = "Wrong name. Should contain only letters";
-        everythingAlright[1] = false;
-        submit.setAttribute("disabled", "");
-        }
-        else{
-            lastname.className = "alright";
-            alert_lastname.style.display = "none";
-            everythingAlright[1] = true;
-            if(everythingAlright.every(function(check){return check===true})){
-                    submit.removeAttribute("disabled");
-                }
-        }
-    });
-    
-    
-    const alert_username = document.getElementById("alert_username");
-    const username = document.getElementById("login");
-    username.addEventListener("change", function(ev){
-        let value = username.value;
+    }
+
+};
+
+function valideName(value){
+     return regexLetters.test(value);
+    };
+
+function valideUsername(value){
         let xhr = new XMLHttpRequest();
         let request = 'https://infinite-hamlet-29399.herokuapp.com/check/' + value;
         xhr.onreadystatechange = function(){
@@ -69,87 +42,110 @@ function attach_events(){
                 if(xhr.status == OK){
                     let response = JSON.parse(xhr.responseText);
                     if (response[value] === "available"){
-                        alert_username.style.display = "none";
-                        username.className = "alright";
-                        everythingAlright[2] = true;
-                        if(everythingAlright.every(function(check){return check===true})){
-                    submit.removeAttribute("disabled");
+                        return true;
                 }
                     } else {
-                        alert_username.style.display = "block";
-                        username.className = "error";
-                        alert_username.innerHTML = "Taken";
-                        everythingAlright[2] = false;
-                        submit.setAttribute("disabled", "");
+                        return false;
                         
                     }
                 } else {
-                    alert_username.style.display = "block";
-                    username.className = "Cannot connect";
+                    return false;
                     
                 }
             }
-        };
         console.log(request);
         xhr.open('GET', request, true);
         xhr.send(null);
-        
+};
+
+function valideGender(value){
+    if (value === 'M' || value === 'F') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validePassword(value){
+    return value.length >= 8 ? true : false;
+};
+
+function valideSecondPassword(value){
+    return value === originValue;
+};
+
+function valideAvatar(value){
+    let extension = value.split('.').pop();
+    let allowedExtensions = ["png", "jpg"];
+    console.log(allowedExtensions, extension);
+    if (allowedExtensions.includes(extension)){
+        return true;
+    } else {
+        return false;
+    }
+};
+
+
+
+
+function attach_events(){
+
+    everythingAlright[3] = true;
+
+    const alert_name = document.getElementById("alert_name");
+    const firstname = document.getElementById("firstname");
+    firstname.addEventListener("change", function(ev){
+        let message = "Name can contain only letters";
+        checkValide(firstname, 0, alert_name, message, valideName);
+    });
+
+
+    const alert_lastname = document.getElementById("alert_lastname");
+    const lastname = document.getElementById("lastname");
+    lastname.addEventListener("change", function(ev){
+        let message = "Last name can contain only letters";
+        checkValide(lastname, 1, alert_lastname, message, valideName);
+    });
+   
+    
+    const alert_username = document.getElementById("alert_username");
+    const username = document.getElementById("login");
+    username.addEventListener("change", function(ev){
+        let message = "Username already taken";
+        checkValide(username, 2, alert_username, message, valideUsername);
+    });
+    
+    everythingAlright[3] = true;
+    /*const alert_gender = document.getElementById("alert_gender");
+    const gender = document.getElementById("gender");
+    gender.addEventListener("change", function(ev){
+        let message = "Gender not chosen";
+        checkValide(gender, 3, alert_gender, message, valideGender);
+    });*/
+    
+    const alert_pass = document.getElementById("alert_pass");
+    const pass1 = document.getElementById("password");
+    pass1.addEventListener("change", function(ev){
+        originValue = pass1.value;
+       let message = "Short password";
+       checkValide(pass1, 4, alert_pass, message, validePassword); 
     });
     
     const alert_pass2 = document.getElementById("alert_pass2");
-    
-    const pass1 = document.getElementById("password");
     const pass2 = document.getElementById("password_second");
-    
     pass2.addEventListener("change", function(ev){
-        let originValue = pass1.value;
-        let newValue = pass2.value;
-        if(!(newValue === originValue)){
-            alert_pass2.style.display = "block";
-            alert_pass2.innerHTML = 'Password does not match';
-            pass2.className = 'error';
-            everythingAlright[4] = false;
-            submit.setAttribute("disabled", "");
-        } else {
-            alert_pass2.style.display = "none";
-            pass2.className = "alright";
-            everythingAlright[4] = true;
-            if(everythingAlright.every(function(check){return check===true})){
-                    submit.removeAttribute("disabled");
-                }
-        
-        }
-    
-    })
-    
+        var originValue = pass1.value;
+        let message = "Password does not match";
+        checkValide(pass2, 5, alert_pass2, message, valideSecondPassword);
+    });
     
     
     const alert_avatar = document.getElementById("alert_avatar");
     const avatar = document.getElementById("photo");
     avatar.addEventListener("change", function(ev){
-        let value = avatar.value;
-        let extension = value.split('.').pop();
-        if (extension === "png" || extension == "jpg" ){
-            alert_avatar.style.display = "none";
-            avatar.className = "alright";
-            everythingAlright[5] = true;
-            if(everythingAlright.every(function(check){return check===true})){
-                    submit.removeAttribute("disabled");
-                }
-        } else {
-            alert_avatar.style.display = "block";
-            alert_avatar.innerHTML = 'Wrong extension';
-            avatar.className = "error";
-            everythingAlright[5] = false;
-            submit.setAttribute("disabled", "");
-    
-        }
-    
-    
-    })
-    
-    
-    
+        let message = "Wrong extension";
+        checkValide(avatar, 6, alert_avatar, message, valideAvatar);
+    });
     
 
 }
